c11e52c6dfef838b41244eae9aaa2b9e4279aae9
Joey Lieb <email@joey.now>
2025-11-08 20:24:56 -0500

    Added NextJS

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+export default function Page() {
+    return (
+
+        <div>
+            <h1 className="">AskThem</h1>
+            <p>Curiosity is finally meeting anonymity</p>
+        </div>
+    )
+}
